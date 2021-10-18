d93887dbff444f719b3ec03fa311c6b1ce1afc10
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 10:47:59 +0530

    C#

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Class_Implementation
+{
+    partial class LMS : IAssesment
+    {
+        string name;
+        int SNo;
+        long MobileNo;
+        string Email;
+        public void Display()
+        {
+            Console.WriteLine("Implementation Void");
+        }
+
+        void IAssesment.MCQ_Assesment()
+        {
+            Console.WriteLine("this implementation from LMS");
+        }
+
+        void IAssesment.QuestionBank()
+        {
+            Console.WriteLine("retuening value from quetion bank");
+        }
+    }
+    partial class LMS : placement, IAssesment
+    {
+        public void Message()
+        {
+            Console.WriteLine("This message is coming from second imp");
+        }
+    }
+    abstract class placement
+    {
+        string Name_of_company;
+        int No_of_student_placed;
+        DateTime Year_of_placement;
+
+        public void placement_record()
+        {
+            Console.WriteLine("This method will show the record of placement");
+        }
+    }
+    public interface IAssesment
+    {
+        public void MCQ_Assesment();
+        public void QuestionBank();
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo_Class_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LMS Akash = new LMS();
+            Akash.Display();
+            Akash.Message();
+            Akash.placement_record();
+
+
+            IAssesment Delhi = Akash;
+            Delhi.MCQ_Assesment();
+            Delhi.QuestionBank();
+
+
+        }
+    }
+}
